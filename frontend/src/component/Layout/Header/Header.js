4622d7c8dcbcc1129f69a26d351225fc7d538c67
legend <tomybryant1@gmail.com>
2023-05-22 03:43:41 +0000

    pro

--- frontend/src/component/Layout/Header/Header.js
+++ frontend/src/component/Layout/Header/Header.js
@@ -1,152 +1,45 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
-import SearchIcon from '@mui/icons-material/Search';
-import "./Header.css";
-import User from "../../../images/user.svg";
-import Compare from "../../../images/compare.svg";
-import Cart from "../../../images/cart.svg";
-import Wishlist from "../../../images/wishlist.svg"
+import React from "react";
+import { ReactNavbar } from "overlay-navbar";
+import logo from "../../../images/logo.png";
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 
-
+const options = {
+  burgerColorHover: "#eb4034",
+  logo,
+  logoWidth: "20vmax",
+  navColor1: "white",
+  logoHoverSize: "10px",
+  logoHoverColor: "#eb4034",
+  link1Text: "Home",
+  link2Text: "Products",
+  link3Text: "Contact",
+  link4Text: "About",
+  link1Url: "/",
+  link2Url: "/products",
+  link3Url: "/contact",
+  link4Url: "/about",
+  link1Size: "1.3vmax",
+  link1Color: "rgba(35, 35, 35,0.8)",
+  nav1justifyContent: "flex-end",
+  nav2justifyContent: "flex-end",
+  nav3justifyContent: "flex-start",
+  nav4justifyContent: "flex-start",
+  link1ColorHover: "#eb4034",
+  link1Margin: "1vmax",
+  profileIconUrl: "/login",
+  profileIconColor: "rgba(35, 35, 35,0.8)",
+  searchIconColor: "rgba(35, 35, 35,0.8)",
+  cartIconColor: "rgba(35, 35, 35,0.8)",
+  profileIconColorHover: "#eb4034",
+  searchIconColorHover: "#eb4034",
+  cartIconColorHover: "#eb4034",
+  cartIconMargin: "1vmax",
+  profileIcon:true,
+  profileIconElement: <PermIdentityIcon/>,
+};
 
 const Header = () => {
-  return (
-    <>
-      <header className="header-top-strip">
-
-        <div className="containerxxl">
-          <div className="Row">
-            <div className="col6">
-              <p className="header-top">
-                Free Shipping Over $100 & Free  Returns
-              </p>
-            </div>
-            <div className="col6">
-              <p className="header-top">
-                Hotline: <a className="textwhite" href="tel:+233 (0) 54 740 3691">+233 (0) 54 740 3691</a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </header>
-
-
-
-      <header className="header-upper">
-        <div className="container-xxl">
-          <div className="Row">
-            <div className="col2">
-              <h2>
-                <Link className="text-white">Logic Gate</Link>
-              </h2>
-
-            </div>
-            <div className="col5">
-              <div class="input-group">
-                <input type="text" className="form-control" placeholder="Search Product Here..."
-                  aria-label="Search Product Here..." aria-describedby="basic-addon2" />
-                <div className='searchIconBox'>
-                  <span className="input-group-text"
-                  ><SearchIcon className="searchIcon" />
-                  </span>
-                </div>
-
-              </div>
-            </div>
-
-
-
-            <div className="col5">
-              <div className="header-upper-link">
-
-
-                <div>
-
-
-                  <Link to="/login" className="usercomponents">
-                    <img src={User} alt="user" />
-                    <p className="mb-0">Login<br /> My Account</p>
-                  </Link>
-                </div>
-
-                <div>
-                  <Link to="/compare-product"
-                    className="usercomponents">
-                    <img src={Compare} alt="Compare" />
-                    <p className="mb-0">Compare <br /> Products</p>
-                  </Link>
-                </div>
-                <div>
-
-
-                  <Link to="/wishlist"
-                    className="usercomponents">
-                    <img src={Wishlist} alt="wishlist" />
-                    <p className="mb-0">Favourite <br /> wishlist</p>
-                  </Link>
-                </div>
-
-                <div>
-
-
-                  <Link to="/cart" className="usercomponents">
-                    <img src={Cart} alt="cart" />
-                    <div className="cartContent">
-                      <span className="badge bg-white text-dark">0</span>
-                      <p className="mb-0">$ 500</p>
-                    </div>
-                    <p className="mb-0"></p>
-                  </Link>
-                </div>
-
-
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-      </header>
-      <header className="header-bottom">
-        <div className="containerxxl">
-          <div className="Row">
-            <div className="col12">
-              <div className="menu-buttom ">
-
-                <div>
-                  <div className="dropdown">
-                    <button className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center"
-                      type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      <img src="images/menu.svg" alt="" />
-                      <span className="ShopCat">Shop Categories</span>
-                    </button>
-
-
-                  </div>
-
-                </div>
-                <div className="menu-links">
-                  <div className="menubottom">
-                    <NavLink to="/" >Home</NavLink>
-                    <NavLink to="/products">Our Store</NavLink>
-                    <NavLink to="/about">Blogs</NavLink>
-                    <NavLink to="/contact">Contact</NavLink>
-
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-      </header>
-
-
-
-    </>
-  );
-}
+  return <ReactNavbar {...options} />;
+};
 
 export default Header;
-
